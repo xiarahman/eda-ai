637119f398d70a46352dbb226db0254f12a630c1
Tehreem-personal <tehreemawan3@gmail.com>
2024-02-21 13:17:49 +0500

    Updated global  Sentiment ProgressBar

--- src/components/sentiments/progress.tsx
+++ src/components/sentiments/progress.tsx
@@ -1,40 +1,30 @@
 import React from "react";
 import { Progress } from "antd";
-
 const GlobalSentimentProgress = ({ sentimentData }) => {
-  if (!sentimentData || sentimentData.length < 3) {
+  if (!sentimentData || sentimentData.length < 1) {
     return null;
   }
-  
-const {percentage, sentiment}= sentimentData[0];
-
-const formattedPercentage= Math.floor(percentage) || 0;
-  // const formattedPercentageForNegative = Math.floor(sentimentData[0].percentage) || 0;
-  // const formattedPercentageForNeutral = Math.floor(sentimentData[1].percentage) || 0;
-  // const formattedPercentageForPositive = Math.floor(sentimentData[2].percentage) || 0;
-
-  return (
-    <>
-      <h3 className="card-heading">{sentiment}</h3>
-      <Progress
-        percent={formattedPercentage}
-        status="active"
-        strokeColor="#169038"
-        style={{ minWidth: 300 }}
-      />
-      {/* <h3 className="card-heading">Neutral</h3> */}
-      {/* <Progress
-        percent={formattedPercentageForNeutral}
-        status="active"
-        strokeColor="#C1CFED"
-      />
-      <h3 className="card-heading">Negative</h3>
-      <Progress
-        percent={formattedPercentageForNegative}
-        status="active"
-        strokeColor="#F53C2B"
-      /> */}
-    </>
+    return (
+      <>
+        {sentimentData.map((sentimentItem, index) => (
+          <div key={index}>
+            <h3 className="card-heading">{sentimentItem.sentiment}</h3>
+            <Progress
+              percent={Math.floor(sentimentItem.percentage) || 0}
+              status="active"
+              strokeColor={
+                sentimentItem.sentiment === "positive"
+                  ? "#169038"
+                  : sentimentItem.sentiment === "neutral"
+                  ? "#C1CFED"
+                  : "#F53C2B"
+              }
+              style={{ minWidth: 300 }}
+            />
+          </div>
+        ))}
+      </>
+   
   );
 };
 
